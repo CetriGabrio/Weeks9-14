74070557b1b3c6e52585714d338340e1f1bf017d
ilacqua <ilacqua@OA-S138-17>
2025-03-31 12:04:40 -0400

    Cleaned the code

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -10,11 +10,17 @@ public class Spaceship : MonoBehaviour
     void Start()
     {
         //Reset player position at the start of the game to the centre of the screen
-        transform.position = new Vector3(0, 0, 0);
+        transform.position = new Vector3(0, -2, 0);
     }
 
     // Update is called once per frame
     void Update()
+    {
+        SpaceshipMovement();
+    }
+
+    //Created a new function for all the spaceship movement features
+    void SpaceshipMovement()
     {
         //Variables for the player horizontal and vertical movement
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -24,5 +30,31 @@ public class Spaceship : MonoBehaviour
         //Since it's a shoot em up game, I do not want the player to be able to rotate and point in any direction other than upwards
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
         transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+
+        //Creating player bouds so that the spaceship is enclosed in the screen
+
+        //This part is handling the vertical movement, so the player can only stay in the bottom side of the screen
+        if (transform.position.y >= -1f)
+        {
+            transform.position = new Vector3(transform.position.x, -1f, 0);
+        }
+        else if (transform.position.y <= -4f)
+        {
+            transform.position = new Vector3(transform.position.x, -4f, 0);
+        }
+
+        //This part is handling the horizontal movement, implementing a pac man effect that allows the player to teleport on the other border of the screen
+        if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
+
+        /////////////////
+        ///Use camera to wolrd point to handle different aspect ratios
+        ////////////////
     }
 }
