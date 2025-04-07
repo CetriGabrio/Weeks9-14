549faf827cd2aa1b022abd1316502b60e8ad7418
ilacqua <ilacqua@OA-S138-16>
2025-04-07 11:31:08 -0400

    Fixed the enemy movement behaviour

--- Assets/Scripts/System Scripts/Enemy.cs
+++ Assets/Scripts/System Scripts/Enemy.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    //Variable for the falling down speed of the enemy
     public float speed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +17,14 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Function to move the enemy only downwards at a constant speed
         transform.Translate(Vector3.down * speed * Time.deltaTime);
 
-        if (transform.position.y >= -6f)
+        //Pac-Man effect once again, this time vetically to ensure that if the enemy reaches the bottom of the screen, it teleports to the top
+        if (transform.position.y < -6.2f)
         {
-            transform.position = new Vector3(0, 6f, 0);
+            //Added a random range on the X so that the position of the teleported enemy is unpredictable
+            transform.position = new Vector3(Random.Range(-9.5f, 9.5f), 6.2f, 0);
         }
     }
 }
