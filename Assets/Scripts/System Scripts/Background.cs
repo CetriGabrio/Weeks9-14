cb107a9cb233fec1e849c205778cb6f7020297a3
Gabriele <gabriele.ilacqua@icloud.com>
2025-04-09 06:19:34 -0400

    Added a scrolling background and sorted the various layers

---
+++ Assets/Scripts/System Scripts/Background.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public float scrollSpeed = 0.1f; 
+
+    private Vector2 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        transform.position = startPosition + Vector2.down * newPosition;
+    }
+}
